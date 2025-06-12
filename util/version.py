34dcb3ee74a9db1e2170c5e67d996eb7886dbac0
Sander Sluis <sander@specto.nl>
2025-06-12 11:03:10 +0200

    docs: add version.py doc string

--- util/version.py
+++ util/version.py
@@ -1,7 +1,15 @@
+"""Retrieve the project version from the pyproject.toml file."""
+
 import tomllib
 
 
 def get_project_version():
+    """Retrieve the project version from the pyproject.toml file.
+    Must be run from the project root directory.
+
+    Returns:
+        str: The version of the project as specified in pyproject.toml.
+    """
     pyproject_path = "pyproject.toml"
 
     with open(pyproject_path, "rb") as f:
