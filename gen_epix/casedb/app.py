f22e5e8c9bb601caa44c43150699673c80bb8916
Sander Sluis <sander@specto.nl>
2025-08-15 09:23:17 +0200

    fix: fix get_project_version

--- gen_epix/casedb/app.py
+++ gen_epix/casedb/app.py
@@ -2,7 +2,7 @@ from gen_epix.casedb.app_setup import create_fast_api
 from gen_epix.casedb.domain import enum
 from gen_epix.casedb.env import AppEnv
 from util.cfg import AppCfg
-from util.util import get_package_version
+from util.version import get_project_version
 
 APP_NAME = "CASEDB"
 
@@ -12,7 +12,7 @@ SCHEMA_KWARGS = {
     "title": "Gen-EpiX",
     "summary": "Genomic Epidemiology platform for disease X",
     "description": "Gen-EpiX is platform for visualizing and analyzing genomic epidemiology data. It has fine-grained access controls for collaboration between multiple organizations.",
-    "version": get_package_version(),
+    "version": get_project_version(),
     "contact": {
         "name": "RIVM CIb IDS bioinformatics group",
         "url": "https://github.com/RIVM-bioinformatics/gen-epix-api",
