24cfa2359f03917ab557cc3c5ab6b32c8db40171
ivwalle <40541576+ivwalle@users.noreply.github.com>
2025-08-17 09:10:18 +0200

    refactor: remove constructor reimplementations for concrete SA repositories (#65)
    Co-authored-by: Ivo van Walle <ivo.van.walle@rivm.nl>

--- gen_epix/seqdb/domain/repository/seq.py
+++ gen_epix/seqdb/domain/repository/seq.py
@@ -5,15 +5,11 @@ import numpy as np
 
 from gen_epix.fastapp import BaseRepository
 from gen_epix.fastapp.unit_of_work import BaseUnitOfWork
-from gen_epix.seqdb.domain import DOMAIN
 from gen_epix.seqdb.domain import model as model  # forces models to be registered now
-from gen_epix.seqdb.domain.enum import ServiceType
 
 
 class BaseSeqRepository(BaseRepository):
-    ENTITIES = DOMAIN.get_dag_sorted_entities(
-        service_type=ServiceType.SEQ, persistable=True
-    )
+    pass
 
     @abc.abstractmethod
     def get_distance_matrix_by_seq_ids(
