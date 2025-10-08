00361f50fe28c7ee5631f13027d44e8cd798e945
Emile van Passel <emile.van.passel@rivm.nl>
2025-10-08 14:56:22 +0200

    test: casedb.case_access data update

--- test/casedb/integration/case_access/test_casedb_case_access.py
+++ test/casedb/integration/case_access/test_casedb_case_access.py
@@ -107,7 +107,7 @@ class TestCaseAccess(CaseAccessSetup):
             raise ValueError("Case CRUD commands DataFrame is not set.")
         df = df.loc[df["dm.is_active"] == True, :]
         command_idx_to_test = None
-        # command_idx_to_test = {6}  # For debugging, set set of indices, otherwise None
+        # command_idx_to_test = {33}  # For debugging, set set of indices, otherwise None
         n_case_type_cols = 3
         # Sort by index to have correct order
         df = df.sort_values(by="index", axis=0).to_dict(orient="records")
