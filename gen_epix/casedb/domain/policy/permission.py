3fa1c0361f022ab178c5dfca427ffda01d94cdac
MarkStreek <mvdstreek2003@gmail.com>
2026-01-06 09:00:05 +0100

    fix: Removed duplicate CaseTypeSetCrudCommand entry

--- gen_epix/casedb/domain/policy/permission.py
+++ gen_epix/casedb/domain/policy/permission.py
@@ -38,10 +38,7 @@ class RoleGenerator(CommonRoleGenerator):
                 PermissionTypeSet.E,
             ),
             # case with impact on abac
-            (
-                command.CaseTypeSetCrudCommand,
-                PermissionTypeSet.D,
-            ),  # Create/update has no impact on abac
+            # Create/update has no impact on abac
             (command.CaseTypeSetMemberCrudCommand, PermissionTypeSet.CUD),
             (command.CaseTypeSetCaseTypeUpdateAssociationCommand, PermissionTypeSet.E),
             (
