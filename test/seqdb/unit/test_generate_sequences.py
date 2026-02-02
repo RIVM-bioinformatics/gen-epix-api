420b2d088ab6c013c152a67751d52d9fa277f04a
MarkStreek <mvdstreek2003@gmail.com>
2026-02-02 09:38:09 +0100

    test: Add unit tests for generate_random_sequence

---
+++ test/seqdb/unit/test_generate_sequences.py
@@ -0,0 +1,94 @@
+from test.seqdb.seqdb_test_client import SeqdbTestClient, SeqGenerationSettings
+
+from gen_epix.seqdb.domain import model
+
+
+def test_generate_sequences_happy_flow() -> None:
+    settings = SeqGenerationSettings(
+        n_loci=5,
+        locus_length=20,
+        p_locus_deletion=0.01,
+        p_nucleotide_substitution=0.02,
+        p_nucleotide_deletion=0.005,
+        seed=1001,
+    )
+    batch = SeqdbTestClient.generate_random_sequences(
+        n_seqs=10,
+        settings=settings,
+    )
+
+    assert isinstance(batch, model.SampleBatchForUpload)
+    assert len(batch.samples) == 10
+    assert all(isinstance(sample, model.SampleForUpload) for sample in batch.samples)
+
+
+def test_generate_sequences_reproducibility() -> None:
+    settings = SeqGenerationSettings(
+        n_loci=5,
+        locus_length=20,
+        p_locus_deletion=0.01,
+        p_nucleotide_substitution=0.02,
+        p_nucleotide_deletion=0.005,
+        seed=1001,
+    )
+    batch1 = SeqdbTestClient.generate_random_sequences(
+        n_seqs=10,
+        settings=settings,
+    )
+    batch2 = SeqdbTestClient.generate_random_sequences(
+        n_seqs=10,
+        settings=settings,
+    )
+
+    seqs1 = [
+        contig.seq
+        for sample in batch1.samples
+        for seq in (sample.seqs or [])
+        for contig in getattr(seq, "contigs", [])
+    ]
+
+    seqs2 = [
+        contig.seq
+        for sample in batch2.samples
+        for seq in (sample.seqs or [])
+        for contig in getattr(seq, "contigs", [])
+    ]
+
+    assert seqs1 == seqs2
+
+
+def test_generate_sequences_uniqueness() -> None:
+    settings1 = SeqGenerationSettings(
+        n_loci=5,
+        locus_length=20,
+        seed=1001,
+    )
+    batch1 = SeqdbTestClient.generate_random_sequences(
+        n_seqs=5,
+        settings=settings1,
+    )
+    settings2 = SeqGenerationSettings(
+        n_loci=5,
+        locus_length=20,
+        seed=1002,
+    )
+    batch2 = SeqdbTestClient.generate_random_sequences(
+        n_seqs=5,
+        settings=settings2,
+    )
+
+    seqs1 = [
+        contig.seq
+        for sample in batch1.samples
+        for seq in (sample.seqs or [])
+        for contig in getattr(seq, "contigs", [])
+    ]
+
+    seqs2 = [
+        contig.seq
+        for sample in batch2.samples
+        for seq in (sample.seqs or [])
+        for contig in getattr(seq, "contigs", [])
+    ]
+
+    assert seqs1 != seqs2
