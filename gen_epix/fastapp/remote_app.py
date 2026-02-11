0d767d94c18e5a53b34cddb029f774c5d9f872bd
MarkStreek <mvdstreek2003@gmail.com>
2026-02-11 11:26:18 +0100

    fix: Fix url construction for fastapp remote_app

--- gen_epix/fastapp/remote_app.py
+++ gen_epix/fastapp/remote_app.py
@@ -248,18 +248,14 @@ class RemoteApp(App):
             match cmd.operation:
                 case CrudOperation.READ_ALL:
                     if cmd.query_filter:
-                        if cmd.props.get("return_id", False):
-                            query_suffix = query_route_suffix.rstrip("/")
-                            ids_suffix = (
-                                ids_route_suffix
-                                if ids_route_suffix.startswith("/")
-                                else ("/" + ids_route_suffix)
-                            )
-                            url = base_route + query_suffix + ids_suffix
-                        else:
-                            url = base_route + query_route_suffix
                         response = client.post(
-                            url,
+                            base_route
+                            + query_route_suffix
+                            + (
+                                "/" + ids_route_suffix
+                                if cmd.props.get("return_id", False)
+                                else ""
+                            ),
                             json=json.loads(cmd.query_filter.model_dump_json()),
                             headers=headers,
                         )
