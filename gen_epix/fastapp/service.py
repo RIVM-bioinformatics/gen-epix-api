cd4caf733ec377d4cbd472ab870fbd0062c9fa25
Ivo van Walle <ivo.van.walle@rivm.nl>
2026-03-20 08:34:23 +0100

    refactor: minor changes after review

--- gen_epix/fastapp/service.py
+++ gen_epix/fastapp/service.py
@@ -4,7 +4,7 @@ import abc
 import datetime
 import logging
 from collections.abc import Callable, Hashable, Iterable
-from typing import Any, Generic, TypeVar
+from typing import Any
 
 from gen_epix.fastapp import exc
 from gen_epix.fastapp.app import App
@@ -21,17 +21,15 @@ from gen_epix.fastapp.repository import BaseRepository
 from gen_epix.fastapp.unit_of_work import BaseUnitOfWork
 from gen_epix.filter import CompositeFilter, LogicalOperator
 
-R = TypeVar("R", bound=BaseRepository, default=BaseRepository)
 
-
-class BaseService(abc.ABC, Generic[R]):
+class BaseService[Repository: BaseRepository = BaseRepository](abc.ABC):
     SERVICE_TYPE: Hashable = None
 
     def __init__(
         self,
         app: App,
         service_type: Hashable = None,  # TODO: service_type this required
-        repository: R | None = None,
+        repository: Repository | None = None,
         logger: logging.Logger | None = None,
         setup_logger: logging.Logger | None = None,
         id: str | None = None,
@@ -52,7 +50,7 @@ class BaseService(abc.ABC, Generic[R]):
         self._service_type = service_type
         self._created_at: datetime.datetime = self._timestamp_factory()
         self._app: App = app
-        self._repository: R | None = repository
+        self._repository: Repository | None = repository
         self._logger: logging.Logger | None = logger
         self._setup_logger: logging.Logger | None = setup_logger
         self._props: dict[str, Any] = props or {}
@@ -107,13 +105,13 @@ class BaseService(abc.ABC, Generic[R]):
         self._logger = logger
 
     @property
-    def repository(self) -> R:
+    def repository(self) -> Repository:
         if not self._repository:
             raise exc.ServiceException("Repository not set")
         return self._repository
 
     @repository.setter
-    def repository(self, repository: R) -> None:
+    def repository(self, repository: Repository) -> None:
         self._repository = repository
 
     @property
