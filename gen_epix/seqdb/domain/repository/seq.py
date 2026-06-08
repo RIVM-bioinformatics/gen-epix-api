b089a4e82a353a2eb2652b8f4d457a51db2cac8b
Paul Essers <paul.essers@rivm.nl>
2026-06-08 11:57:39 +0200

    refactor(seqdb): LSP-3359 replace get_sample_identifiers_by_sample_ids with standard crud filter

--- gen_epix/seqdb/domain/repository/seq.py
+++ gen_epix/seqdb/domain/repository/seq.py
@@ -144,17 +144,6 @@ class BaseSeqRepository(BaseRepository):
         """
         raise NotImplementedError()
 
-    # TODO [LSP-3359] use standard crud method with filter rather than dedicated method
-    @abc.abstractmethod
-    def get_sample_identifiers_by_sample_ids(
-        self,
-        sample_ids: list[UUID],
-    ) -> list[model.SampleIdentifier]:
-        """
-        Retrieve only SampleIdentifier records for the given sample IDs.
-        """
-        raise NotImplementedError()
-
     @abc.abstractmethod
     def update_some_seq_distance_content(
         self,
