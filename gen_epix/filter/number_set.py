a28d5b2927920c03cb4f300e9f2e98effc6b014e
Ivo van Walle <ivo.van.walle@rivm.nl>
2026-07-29 21:24:35 +0200

    feat(fastapp): DEFAULT_ROOT_TOKEN_TIME_TO_LIVE added again and behaviour changed (TTL zero means not applied) test: added meaningful error to test.all when some tests fail

--- gen_epix/filter/number_set.py
+++ gen_epix/filter/number_set.py
@@ -3,11 +3,11 @@ from typing import Literal
 
 from pydantic import Field
 
-from gen_epix.filter.base import Filter
 from gen_epix.filter.enum import FilterType
+from gen_epix.filter.hashable_set import HashableSetFilter
 
 
-class NumberSetFilter(Filter):
+class NumberSetFilter(HashableSetFilter):
     members: frozenset[int | float | Decimal] = Field(
         description="The numbers to match.", frozen=True
     )
