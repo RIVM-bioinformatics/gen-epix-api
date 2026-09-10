bdf12303617f00208b76069ebf92dd6112210a77
Ivo van Walle <ivo.van.walle@rivm.nl>
2026-09-10 05:17:53 +0200

    refactor(fastapp): minor changes after PR review

--- gen_epix/fastapp/services/auth/oauth_idp_client.py
+++ gen_epix/fastapp/services/auth/oauth_idp_client.py
@@ -214,7 +214,17 @@ class OauthIdpClient(IdpClient, OpenIdConnect):
 
     async def get_jwk_from_jwt(self, jwt_token: str) -> jwt.PyJWK:
         """Return jwk from jwt."""
-        key_id: str = self._validate_key_id(jwt_token, self._parse_kid(jwt_token))
+        key_id = self._parse_kid(jwt_token)
+        if not key_id:
+            if self.logger:
+                self.logger.warning(
+                    self._log_item_class(
+                        code="0184bc35",
+                        msg="No key ID found in token header",
+                        scheme_name=self.scheme_name,
+                    ).dumps()
+                )
+            raise exc.UnauthorizedAuthError("d3d0bb67")
 
         # Verify that the signing key in this session is outdated, fetch new one if so
         # TODO: verify if fetching new signing keys is ok
@@ -263,20 +273,6 @@ class OauthIdpClient(IdpClient, OpenIdConnect):
                 ).dumps()
             )
         self._load_keys()
-
-    def _validate_key_id(self, jwt_token: str, key_id: str | None) -> str:
-        """Validate key id."""
-        if not key_id:
-            if self.logger:
-                self.logger.warning(
-                    self._log_item_class(
-                        code="0184bc35",
-                        msg="No key ID found in token header",
-                        scheme_name=self.scheme_name,
-                    ).dumps()
-                )
-            raise exc.UnauthorizedAuthError("d3d0bb67")
-        return key_id
 
     def _parse_kid(self, jwt_token: str) -> str | None:
         """Parse kid."""
